c8cf07f5a35c802af1145546c7ce48b531ba49a8
yjy1 <318277446@qq.com>
2023-01-08 13:34:06 +0800

    tag_Mustache语法细节

---
+++ pages/wxml/wxml.js
@@ -0,0 +1,81 @@
+// pages/wxml/wxml.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    message:"你好，效果呈现",
+    firstName:'kobe',
+    lastName:'bryant',
+    age:13,
+    // time:new Date().toLocaleString()
+    time:"",
+    isActive:true
+  },
+  handleBtnClick(){
+    this.setData({
+      isActive:!this.data.isActive
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    setInterval(() => { //定时器，setTimeOut不起作用
+      console.log('setInterval')
+      this.setData({
+        time:new Date().toLocaleString()
+      })
+    }, 1000);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
