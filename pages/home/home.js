44cf4a7ef9cf4effc20d2c63a60f940d3257f47e
yjy1 <318277446@qq.com>
2023-01-09 18:40:46 +0800

    tag_小程序中展示弹窗

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,54 +1,59 @@
 // pages/home/home.js
-import request from '../../service/network.js'
 Page({
-  data: {
-
-  },
-  onLoad(options) {
-    // --------------------1.原生发送网络请求-------------------
-    this.get_data_origin()
-
-    // --------------------2.使用封装的request发送网络请求-------------------
-    // Promise最大的好处是防止出现回调地狱
-    request({
-      url:"http://localhost:8888/userlist",
-    }).then((res)=>{
-      console.log(res)
-    }).catch((err)=>{
-      console.log(err)
+  handleShowToast(){
+    console.log('aaaaaaaa')
+    wx.showToast({
+      title: '我是弹窗',
+      duration:3000,
+      icon:"loading",
+      mask:true,//不允许点击页面元素
+      success:(res)=>{
+        console.log(res)
+      },
+      fail:(err)=>{
+        console.log(err)
+      },
+      complete:(res)=>{
+        console.log('complete---------',res)
+      }
+    })
+  } ,
+  handleShowModal(){
+    wx.showModal({
+      title: '我是Modal',
+      content: '我是Modal里的内容',
+      showCancel:true,
+      cancelText:"退出",
+      cancelColor:"orange",
+      complete: (res) => {
+        if (res.cancel) {
+          console.log('cancel')
+        }
+    
+        if (res.confirm) {
+          console.log('confirm')
+        }
+      }
     })
   },
-  get_data_origin(){
-    // 1.发送最简单的get请求get
-    // wx.request({
-    //   url: 'http://localhost:8888/userlist',
-    //   data:{ //携带参数
-    //     type:'住宅v'
-    //   },
-    //   success:(res)=>{
-    //     console.log(res)
-    //   }
-    // })
-
-    // 2.post请求
-    // wx.request({
-    //   url: 'http://localhost:8888/userlist',
-    //   method:'POST',
-    //   data:{
-    //     "id": 10087,
-    //     "title": "海天家园2023",
-    //     "type": "住宅2023",
-    //     "num": "1-1-2",
-    //     "hometype": "三室两厅",
-    //     "name": "关羽"
-    //   },
-    //   success:(res)=>{
-    //     console.log('success-----------',res)
-    //   },
-    //   // 失败的情况
-    //   fail:(err)=>{
-    //     console.log('fail-----------',err)
-    //   }
-    // })
+  handleShowLoading(){
+    wx.showLoading({
+      title: '我是loading',
+      mask:true,
+      success:()=>{
+        setTimeout(() => {
+          wx.hideLoading() // 关闭loading
+        }, 3000);
+      }
+    })
+  },
+  handleShowActionSheet(){
+    wx.showActionSheet({
+      itemList: ['方案1','方案2','方案3'],
+      itemColor:"crimson",
+      success:(res)=>{
+        console.log('showActionSheet---',res)
+      }
+    })
   }
 })
