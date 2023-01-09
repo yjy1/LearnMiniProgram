7141bd8c643699a25afa4858b78593510e33a5a3
yjy1 <318277446@qq.com>
2023-01-09 18:15:48 +0800

    tag_网络请求-工具函数封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,10 +1,24 @@
 // pages/home/home.js
+import request from '../../service/network.js'
 Page({
   data: {
 
   },
   onLoad(options) {
-    // 发送网络请求
+    // --------------------1.原生发送网络请求-------------------
+    this.get_data_origin()
+
+    // --------------------2.使用封装的request发送网络请求-------------------
+    // Promise最大的好处是防止出现回调地狱
+    request({
+      url:"http://localhost:8888/userlist",
+    }).then((res)=>{
+      console.log(res)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  },
+  get_data_origin(){
     // 1.发送最简单的get请求get
     // wx.request({
     //   url: 'http://localhost:8888/userlist',
@@ -17,25 +31,24 @@ Page({
     // })
 
     // 2.post请求
-    wx.request({
-      url: 'http://localhost:8888/userlist',
-      method:'POST',
-      data:{
-        "id": 10087,
-        "title": "海天家园2023",
-        "type": "住宅2023",
-        "num": "1-1-2",
-        "hometype": "三室两厅",
-        "name": "关羽"
-      },
-      success:(res)=>{
-        console.log('success-----------',res)
-      },
-      // 失败的情况
-      fail:(err)=>{
-        console.log('fail-----------',err)
-      }
-    })
-  },
- 
+    // wx.request({
+    //   url: 'http://localhost:8888/userlist',
+    //   method:'POST',
+    //   data:{
+    //     "id": 10087,
+    //     "title": "海天家园2023",
+    //     "type": "住宅2023",
+    //     "num": "1-1-2",
+    //     "hometype": "三室两厅",
+    //     "name": "关羽"
+    //   },
+    //   success:(res)=>{
+    //     console.log('success-----------',res)
+    //   },
+    //   // 失败的情况
+    //   fail:(err)=>{
+    //     console.log('fail-----------',err)
+    //   }
+    // })
+  }
 })
