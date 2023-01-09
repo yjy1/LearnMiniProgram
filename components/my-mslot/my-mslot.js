25f42bfd59754aaeb1104fea4eca54e32136e880
yjy1 <318277446@qq.com>
2023-01-09 16:04:28 +0800

    tag_组件化-插槽slot的使用过程

---
+++ components/my-mslot/my-mslot.js
@@ -0,0 +1,27 @@
+// components/my-mslot/my-mslot.js
+Component({
+  options:{
+    // 开启多插槽
+    multipleSlots:true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
