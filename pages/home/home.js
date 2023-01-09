fe3b5bf082000edaa4bbc662ab046a6f6e4b8e26
yjy1 <318277446@qq.com>
2023-01-09 19:20:40 +0800

    tag_小程序使用分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -55,5 +55,13 @@ Page({
         console.log('showActionSheet---',res)
       }
     })
+  },
+  onShareAppMessage(option){
+    console.log(option)
+    return{
+      title:'你好啊',
+      path:'../about/about.wxml',
+      imageUrl:'https://www.lgstatic.com/thumbnail_160x160/i/image3/M01/58/86/CgpOIF34fTmAFX8BAAAXucoZ4a4114.jpg'
+    }
   }
 })
